6475198bad565210c9d9fec420d5830b0e9dac63
tropicalhobo <jose.torres@obf.ateneo.edu>
2016-02-25 00:24:35 +0800

    Skeleton for Random Sampling class

---
+++ mask_test.py
@@ -0,0 +1,69 @@
+__author__ = 'G Torres'
+
+import gdal, ogr
+from gdalconst import *
+import numpy as np
+import numpy.ma as ma
+import os, random
+
+gdal.AllRegister()
+
+def pixel_counter(fn):
+    rs_ds = gdal.Open(fn, GA_ReadOnly)
+
+    if rs_ds is None:
+        return 'Could not open ' + fn
+        sys.exit(1)
+
+    cols, rows = rs_ds.RasterXSize, rs_ds.RasterYSize
+    geo_trans = rs_ds.GetGeoTransform()
+    pix_width, pix_height = geo_trans[1], geo_trans[5]*-1
+    band = rs_ds.GetRasterBand(1)
+
+    #get minimum and maximum of image to create bins
+    #band_min = band.GetMinimum()
+    #band_max = band.GetMaximum()
+    band_hist = band.GetHistogram()
+
+    #pixel counter
+    data = band.ReadAsArray(0,0,
+                            cols,rows)
+
+    #mask array to mask out cloud, cloud shadow and 0 values
+    mdata = ma.masked_where(data==15,data)
+    mcloud = ma.masked_inside(mdata,4,6)
+
+    #land class pixel count and area computation
+    lclass_count = {}
+    lclass_area = {}
+
+    cnt = 1
+    for i in band_hist[:]:
+        lclass_count[cnt] = i
+        lclass_area[cnt] = i*pix_width*pix_height
+        cnt += 1
+
+    #classified pixels total area computation
+    total_cl_area = 0
+    for i in lclass_area:
+        total_cl_area += lclass_area[i]
+
+    #proportion computation
+    class_proportion = {}
+    for j in lclass_area:
+        class_proportion[j] = lclass_area[j]/total_cl_area*100
+
+    return mcloud
+
+def main():
+    import csv
+    print os.getcwd()
+
+    tst_landcover = "TST_LC.tif"
+
+    lc_raster = pixel_counter(tst_landcover)
+
+    print lc_raster
+
+if __name__ == "__main__":
+    main()
