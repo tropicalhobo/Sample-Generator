d051b45d2ad1a3d9ea1a4ee315764092694ea96d
tropicalhobo <jose.torres@obf.ateneo.edu>
2016-05-01 01:03:37 +0800

    renamed .py files

---
+++ sample_generator.py
@@ -0,0 +1,261 @@
+__author__ = 'G Torres'
+
+import gdal
+from gdalconst import *
+import numpy as np
+import numpy.ma as ma
+import random
+import os
+from subprocess import call
+
+
+class RandomSample:
+
+    def __init__(self, f, s_size=500, i_pix=[0, 15], r_path = None, buff_dist = 10):
+
+        if os.path.exists(f) is False:
+            raise ValueError, "file does not exist"
+        self.roads = r_path
+        self.buffer_dist = buff_dist
+        self.file_name = f
+        self.sample_size = s_size
+        self.ignore_pix = i_pix
+
+        # collect image parameters
+        self.img_parameters(f)
+
+        # collect coordinates of random samples
+        #self.get_samples()
+
+        # convert pixel coordinates to map coordinates
+        #self.pix_to_map()
+
+    def metadata(self):
+        class_cat = self.band.GetCategoryNames()
+        color_int = self.band.GetColorInterpretation()
+        color_ent = gdal.GetColorInterpretationName(color_int)
+        pal_name = gdal.GetPaletteInterpretationName(color_int)
+        color_tab = self.band.GetColorTable()
+
+        return color_ent
+
+    def buffer_road(self):
+        import ogr
+
+        b_dist = self.buffer_dist
+        driver = ogr.GetDriverByName('ESRI Shapefile')
+        road_ds = driver.Open(self.roads, 0)
+        lyr = road_ds.GetLayer(0)
+        road_feat = lyr.GetFeature(0)
+        road_geom = road_feat.GetGeometryRef()
+        # TODO: implement geometry type check. abort operation if type not line
+        road_buff = road_geom.Buffer(b_dist)  # buffer road feature
+
+        return road_buff
+
+
+    def clip_dataset(self):
+        pass
+
+    def img_parameters(self, f):
+        gdal.AllRegister()
+        self.raster = gdal.Open(f, GA_ReadOnly)
+        self.cols = self.raster.RasterXSize
+        self.rows = self.raster.RasterYSize
+        self.projection = self.raster.GetProjection()
+        self.geotrans = self.raster.GetGeoTransform()
+        self.band = self.raster.GetRasterBand(1)
+
+        return
+
+    def get_samples(self):
+        """Generates a random sample of coordinates within the desired map classes.
+        Returns list type with tuple elements of pixel coordinates."""
+        ignore_pixel = self.ignore_pix
+        self.data = self.band.ReadAsArray(0, 0, self.cols, self.rows)
+        mask = np.in1d(self.data, ignore_pixel).reshape(self.data.shape)  # returns boolean of ignored values
+        masked_data = ma.array(self.data, mask=mask)  # masks the image-array
+        nonmask_ind = ma.where(masked_data > 0)  # returns the indices of non-masked elements
+
+        self.rand_coord = random.sample(zip(nonmask_ind[0],
+                                       nonmask_ind[1]),
+                                   self.sample_size)
+
+        return
+
+    def pix_to_map(self):
+        """Converts the sample of geographic coordinates to utm projected map coordinates.
+        Returns dict type with tuple elements of geographic and projected coordinates
+        with pixel values"""
+        coord_samples = self.rand_coord
+        topleft_x = self.geotrans[0]
+        topleft_y = self.geotrans[3]
+        pix_width = self.geotrans[1]
+        pix_height = self.geotrans[5]
+
+        self.samples = {}
+
+        from pyproj import Proj, transform
+
+        wgs84 = Proj(proj='latlong', ellps='WGS84')
+        utm51n = Proj(proj='utm', zone=51, ellps='WGS84')
+
+        for coord in coord_samples:
+            x_coord = topleft_x + coord[1]*pix_width
+            y_coord = topleft_y + coord[0]*pix_height
+            x_geo, y_geo = transform(utm51n, wgs84, x_coord, y_coord)
+            self.samples[coord] = (x_coord, y_coord), (x_geo, y_geo), self.data[coord[0], coord[1]]
+
+        return
+
+    def new_csv(self):
+        """Creates a new csv file with current date and time as suffix.
+        Returns string."""
+        import time
+        bn = os.path.basename(self.file_name)
+        t = time.localtime()
+        time_stamp = str(t[0]) + str(t[1]) + str(t[2]) + str(t[3]) \
+              + str(t[4]) + str(t[5]) + str(t[6])
+        new = time_stamp + "_" + bn + "_random_samples" + ".csv"
+
+        return new
+
+    def save_to_csv(self):
+        """Saves samples to a csv file."""
+        import csv
+
+        with open(self.new_csv, 'wb') as csvfile:
+            sample_writer = csv.writer(csvfile, delimiter=',')
+            sample_writer.writerow(['id', 'geog_x', 'geog_y',
+                                    'proj_x', 'proj_y',
+                                    'pix_val'])
+
+            sample_id = 1
+            for i in self.samples:
+
+                sample_writer.writerow([sample_id,  # id number
+                                       self.samples[i][1][0],  # longitude
+                                       self.samples[i][1][1],  # latitude
+                                       self.samples[i][0][0],  # projected x coord
+                                       self.samples[i][0][1],  # projected y coord
+                                       self.samples[i][2]])  # pixel value
+
+                sample_id += 1
+
+        return
+
+
+class StratSample(RandomSample):
+
+    def __init__(self, f, i_pix=[0, 15], prop=5):
+        #RandomSample.__init__(self, f, i_pix)
+
+        if prop <= 100:
+            pass
+        else:
+            raise ValueError, "proportion must be <= 100"
+
+        self.class_proportion = prop
+        self.file_name = f
+        self.ignore_pix = i_pix
+        self.img_parameters(f)
+        #self.stratify_samples()  # perform stratified sampling
+        #self.pix_to_map()  # convert pixel coordinates to map coordinates
+
+    def stratify_samples(self):
+        """Collect random coordinates within classes according to user-specified
+        proportion."""
+        band_hist = self.band.GetHistogram()
+        band_max = self.band.GetMaximum()
+        band_min = self.band.GetMinimum()
+        #stat = self.band.GetStatistics(0, 0)
+
+        class_prop = {}
+        self.rand_coord = {}
+
+        # iterate each class value to perform stratified sampling
+        for pix_val in range(int(band_min), int(band_max)):
+            if pix_val in self.ignore_pix:
+                pass
+            else:
+                class_prop[pix_val] = int((band_hist[pix_val]*  # compute class proportion for sampling
+                                           self.class_proportion)/100)
+                self.data = self.band.ReadAsArray(0, 0, self.cols, self.rows)
+                pix_class = np.in1d(self.data, pix_val).reshape(self.data.shape)  # select pixels from image array
+                pix_loc = np.where(pix_class)  # collect the pixel coordinates of current pixel value
+                pix_coord = random.sample(zip(pix_loc[0], pix_loc[1]),  # collect pixel coordinates randomly using
+                                          class_prop[pix_val])          # class proportion
+                self.rand_coord[pix_val] = pix_coord
+
+        return
+
+    def pix_to_map(self):
+        """Converts the sample of geographic coordinates to utm projected map coordinates."""
+
+        topleft_x = self.geotrans[0]
+        topleft_y = self.geotrans[3]
+        pix_width = self.geotrans[1]
+        pix_height = self.geotrans[5]
+
+        self.strat_samples = {}
+
+        from pyproj import Proj, transform
+
+        wgs84 = Proj(proj='latlong', ellps='WGS84')
+        utm51n = Proj(proj='utm', zone=51, ellps='WGS84')
+
+        for strata in self.rand_coord:
+            for coord in self.rand_coord[strata]:
+                x_coord = topleft_x + coord[1] * pix_width
+                y_coord = topleft_y + coord[0] * pix_height
+                x_geo, y_geo = transform(utm51n, wgs84, x_coord, y_coord)
+                self.strat_samples[coord] = (x_coord, y_coord), (x_geo, y_geo), self.data[coord[0], coord[1]]
+
+        return self.strat_samples
+
+    def new_csv(self):
+        """Creates a new csv file with current date and time as suffix"""
+        import time
+        bn = os.path.basename(self.file_name)
+        t = time.localtime()
+        time_stamp = str(t[0]) + str(t[1]) + str(t[2]) + str(t[3]) \
+                     + str(t[4]) + str(t[5]) + str(t[6])
+        new = time_stamp + "_" + bn + "_strat_samples" + ".csv"
+
+        return new
+
+    def save_to_csv(self):
+        """Saves samples to a csv file."""
+        import csv
+
+        with open(self.new_csv(), 'wb') as csvfile:
+            sample_writer = csv.writer(csvfile, delimiter=',')
+            sample_writer.writerow(['id', 'geog_x', 'geog_y',
+                                    'proj_x', 'proj_y',
+                                    'pix_val'])
+
+            sample_id = 1
+            for i in self.strat_samples:
+                sample_writer.writerow([sample_id,  # id number
+                                        self.strat_samples[i][1][0],  # longitude
+                                        self.strat_samples[i][1][1],  # latitude
+                                        self.strat_samples[i][0][0],  # projected x coord
+                                        self.strat_samples[i][0][1],  # projected y coord
+                                        self.strat_samples[i][2]])  # pixel value
+
+                sample_id += 1
+
+
+def main():
+
+    test_lc = "C:\Users\G Torres\Desktop\GmE205FinalProject\\test_lc"
+    road = "C:\Users\G Torres\Desktop\GmE205FinalProject\\bulacan_roads.shp"
+
+    random_sample = RandomSample(test_lc, r_path=road, buff_dist=10)
+    strat_sample = StratSample(test_lc, i_pix=[0,15], prop=1)
+
+    print random_sample.buffer_road()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sample_generator_prototype.py
@@ -0,0 +1,122 @@
+import gdal
+from gdalconst import *
+import numpy as np
+import numpy.ma as ma
+import os, random
+
+__author__ = 'G Torres'
+
+
+gdal.AllRegister()
+
+
+def pixel_counter(fn):
+    rs_ds = gdal.Open(fn, GA_ReadOnly)
+
+    if rs_ds is None:
+        return 'Could not open ' + fn
+        sys.exit(1)
+
+    cols, rows = rs_ds.RasterXSize, rs_ds.RasterYSize
+    geo_trans = rs_ds.GetGeoTransform()
+    pix_width, pix_height = geo_trans[1], geo_trans[5]*-1
+    band = rs_ds.GetRasterBand(1)
+
+    # get minimum and maximum of image to create bins
+    # band_min = band.GetMinimum()
+    band_max = band.GetMaximum()
+    band_hist = band.GetHistogram()
+
+    # pixel counter
+    data = band.ReadAsArray(0, 0,
+                            cols, rows)
+
+    # land class pixel count and area computation
+    lclass_count = {}
+    lclass_area = {}
+
+    cnt = 1
+    for i in band_hist[1:16]:
+        lclass_count[cnt] = i
+        lclass_area[cnt] = i*pix_width*pix_height
+        cnt += 1
+
+    # classified pixels total area computation
+    total_cl_area = 0
+    for i in lclass_area:
+        total_cl_area += lclass_area[i]
+
+    # proportion computation
+    class_proportion = {}
+    for j in lclass_area:
+        class_proportion[j] = lclass_area[j]/total_cl_area*100
+
+    # return class_proportion
+
+    # mask array to mask out cloud, cloud shadow and 0 values
+    ignore_pix = [4, 5, 6, 15]
+    mask = np.in1d(data, ignore_pix).reshape(data.shape)  # returns boolean of ignored values
+    mdata = ma.array(data,mask=mask)  # masks the image-array
+    nonmask_ind = ma.where(mdata>0)  # returns the indices of non-masked elements
+
+    # random selection of indices of non-masked elements
+    sample_size = 500
+    rand_coord = random.sample(zip(nonmask_ind[0],
+                                   nonmask_ind[1]),
+                               sample_size)
+
+    # convert pixel coordinate to map coordinates TODO: avoid unwanted pixel values
+    map_val = {}
+
+    from pyproj import Proj, transform
+
+    wgs84 = Proj(proj='latlong', ellps='WGS84')
+    utm51n = Proj(proj='utm', zone=51, ellps='WGS84')
+
+    for coord in rand_coord:
+        x_coord = geo_trans[0]+coord[0]*pix_width  # left to right
+        y_coord = geo_trans[3]-coord[1]*pix_height  # top to bottom
+
+        # convert map coordinates to geographic coordinates
+        x_geo, y_geo = transform(utm51n, wgs84, x_coord, y_coord)
+        map_val[coord] = (x_coord, y_coord), (x_geo, y_geo), data[coord[0], coord[1]]
+
+    return map_val
+
+    # memory clean-up
+
+
+def main():
+    import csv
+    #print os.getcwd()
+
+    tst_landcover = "C:\Users\G Torres\Desktop\GmE205FinalProject\\test_lc"
+
+    lc_raster = pixel_counter(tst_landcover)
+
+    
+
+    #print 'Pixel Coordinates: Map Coordinates, Pixel Value'
+
+    
+    count = 1
+    with open('test_sample6.csv', 'wb') as csvfile:
+        sample_writer = csv.writer(csvfile, delimiter=',')
+        sample_writer.writerow(['Geog_x', 'Geog_y',
+                                'Proj_x', 'Proj_y',
+                                'Pix_Val'])
+
+        for i in lc_raster:
+            sample_writer.writerow([lc_raster[i][1][0],  # longitude
+                                   lc_raster[i][1][1],  # latitude
+                                   lc_raster[i][0][0],  # projected x coord
+                                   lc_raster[i][0][1],  # projected y coord
+                                   lc_raster[i][2]])  # pixel value
+
+            print count, '.', i, ':', lc_raster[i][1], \
+                ',', lc_raster[i][0], ',', lc_raster[i][2]
+            count += 1
+    
+    
+if __name__ == "__main__":
+    main()
